6700e769e0797689e4578f966b4da2a7cddbebce
Petra Bayu Pangestu <petrabayu19@gmail.com>
2023-11-22 07:59:19 +0700

    Consultation Page FE Design

--- src/Components/Consultation/ConsultationCarousel.jsx
+++ src/Components/Consultation/ConsultationCarousel.jsx
@@ -5,50 +5,62 @@ import ImageThree from "./images/many-doctor-2.png";
 
 function ConsultationCarousel() {
   return (
-    <Carousel
-      data-bs-theme="dark"
-      controls={false}
-      indicators={false}
-      interval={3000}
-      touch={true}
-      style={{ backgroundColor: "rgba(171,135,255,0.4)", padding: "2rem" }}
-    >
-      <Carousel.Item>
-        <img
-          className="d-block img-fluid mx-auto"
-          src={ImageOne}
-          alt="First slide"
-          style={{ height: "40vh", width: "auto" }}
-        />
-        <div className="text-center">
-          <h3 className="fw-bold">Sumber Yang Terpercaya</h3>
-        </div>
-      </Carousel.Item>
+    <>
+      <h2
+        className="text-center m-0"
+        style={{ backgroundColor: "rgba(171,135,255,0.4)", padding: "2rem" }}
+      >
+        Penuhi Kebutuhan Gizi Anda di Nutrikid
+      </h2>
+      <Carousel
+        data-bs-theme="dark"
+        controls={false}
+        indicators={false}
+        interval={3000}
+        touch={true}
+        style={{
+          backgroundColor: "rgba(171,135,255,0.4)",
+          padding: "2rem",
+          paddingTop: "0px",
+        }}
+      >
+        <Carousel.Item>
+          <img
+            className="d-block img-fluid mx-auto"
+            src={ImageOne}
+            alt="First slide"
+            style={{ height: "40vh", width: "auto" }}
+          />
+          <div className="text-center">
+            <h3 className="fw-bold">Sumber Yang Terpercaya</h3>
+          </div>
+        </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block img-fluid mx-auto"
-          src={ImageTwo}
-          alt="First slide"
-          style={{ height: "40vh", width: "auto" }}
-        />
-        <div className="text-center">
-          <h3 className="fw-bold">Informasi Disampaikan Dengan Cepat</h3>
-        </div>
-      </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block img-fluid mx-auto"
+            src={ImageTwo}
+            alt="First slide"
+            style={{ height: "40vh", width: "auto" }}
+          />
+          <div className="text-center">
+            <h3 className="fw-bold">Informasi Disampaikan Dengan Cepat</h3>
+          </div>
+        </Carousel.Item>
 
-      <Carousel.Item>
-        <img
-          className="d-block img-fluid mx-auto"
-          src={ImageThree}
-          alt="First slide"
-          style={{ height: "40vh", width: "auto" }}
-        />
-        <div className="text-center">
-          <h3 className="fw-bold">Konsultasi Dengan Para Ahli</h3>
-        </div>
-      </Carousel.Item>
-    </Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block img-fluid mx-auto"
+            src={ImageThree}
+            alt="First slide"
+            style={{ height: "40vh", width: "auto" }}
+          />
+          <div className="text-center">
+            <h3 className="fw-bold">Konsultasi Dengan Para Ahli</h3>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 }
 
