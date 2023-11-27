314aa764cf351b822e9ea475c01f62deb9535f06
Petra Bayu Pangestu <petrabayu19@gmail.com>
2023-11-27 20:58:56 +0700

    Update card login register to get data

--- src/Components/LoginRegister/Register.jsx
+++ src/Components/LoginRegister/Register.jsx
@@ -2,9 +2,44 @@ import Media from "react-media";
 import RegisterImage from "./Images/background-item-signup.png";
 import NutrikidLogo from "../../../public/nutrikid-logo/nutrikid-text-only-blue-svg.svg";
 import LeftBox from "./LeftBox";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "./login.css";
 
 export default function Register() {
+  const history = useNavigate();
+
+  const apiRegister = "http://localhost:3000/register";
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Password dan Konfirmasi Password harus sama");
+      return;
+    }
+
+    try {
+      const response = await axios.post(apiRegister, {
+        username,
+        password,
+        email,
+      });
+
+      console.log("Registration Successful:", response.data);
+      history("/login");
+    } catch (error) {
+      console.error("Registration Failed:", error.message);
+      // Mungkin tambahkan logika untuk menampilkan pesan kesalahan kepada pengguna
+    }
+  };
   return (
     <>
       {/* --------------------- MAIN CONTAINER --------------------- */}
@@ -21,7 +56,12 @@ export default function Register() {
           {/*  --------------------- RIGHT BOX ---------------------  */}
           <div className="col-md rounded-4 mx-2 vh-50">
             <div className="">
-              <form id="login-form" action="index.html" className="">
+              <form
+                method="POST"
+                id="login-form"
+                action="index.html"
+                className=""
+              >
                 <img
                   src={NutrikidLogo}
                   alt=""
@@ -30,30 +70,34 @@ export default function Register() {
                 />
                 <h1 className="text-center fs-2">Sign Up</h1>
                 <div className="row mt-2 mb-2">
-                <div className="col-6">
-                  <label className="form-label">Nama Depan</label>
-                  <input
-                    id="fistname"
-                    type="text"
-                    name="firstname"
-                    className="form-control form-control-sm border-success p-2"
-                    placeholder="Nama Depan"
-                    required
-                  />
+                  <div className="col-6">
+                    <label className="form-label">Nama Depan</label>
+                    <input
+                      id="fistname"
+                      type="text"
+                      name="firstname"
+                      className="form-control border-success p-2"
+                      placeholder="Nama Depan"
+                      value={firstName}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="col-6">
+                    <label className="form-label">Nama Belakang</label>
+                    <input
+                      id="lastname"
+                      type="text"
+                      name="lastname"
+                      className="form-control border-success p-2"
+                      placeholder="Nama Belakang"
+                      value={lastName}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
                 </div>
-                <div className="col-6">
-                  <label className="form-label">Nama Belakang</label>
-                  <input
-                    id="lastname"
-                    type="text"
-                    name="lastname"
-                    className="form-control form-control-sm border-success p-2"
-                    placeholder="Nama Belakang"
-                    required
-                  />
-                </div>
-                </div>
-                
+
                 <div className="mb-2">
                   <label className="form-label">Email</label>
                   <input
@@ -62,6 +106,8 @@ export default function Register() {
                     id="email"
                     placeholder="Masukkan Email Anda"
                     pattern=".+[a-zA-Z].+@.+\..{3}"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                   />
                 </div>
@@ -72,6 +118,8 @@ export default function Register() {
                     className="form-control border-success"
                     id="password"
                     placeholder="Buat Password Anda"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     required
                   />
                 </div>
@@ -82,14 +130,17 @@ export default function Register() {
                     className="form-control border-success"
                     id="password"
                     placeholder="Konfirmasi Password Anda"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
                     required
                   />
                 </div>
-
+                {error && <p style={{ color: "red" }}>{error}</p>}
                 <button
                   className="btn btn-primary w-100 py-2  mt-2"
                   id="login-btn"
                   type="submit"
+                  onClick={handleRegistration}
                 >
                   Sign Up
                 </button>
